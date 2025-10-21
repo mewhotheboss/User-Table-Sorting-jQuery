2346c5e56b34ee0ce3556cc81ec6e628fa3b294e
Sakibul Hasan Rafi <iamshrafi@gmail.com>
2025-10-21 18:48:39 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+    const user = [
+        {Name: 'Rafi', Age: 25, Adress: 'Tangail'},
+        {Name: 'Rafi', Age: 25, Adress: 'Tangail'},
+        {Name: 'Rafi', Age: 25, Adress: 'Tangail'},
+        {Name: 'Rafi', Age: 25, Adress: 'Tangail'},
+        {Name: 'Rafi', Age: 25, Adress: 'Tangail'},
+        {Name: 'Rafi', Age: 25, Adress: 'Tangail'},
+        {Name: 'Rafi', Age: 25, Adress: 'Tangail'},
+        {Name: 'Rafi', Age: 25, Adress: 'Tangail'},
+        {Name: 'Rafi', Age: 25, Adress: 'Tangail'},
+        {Name: 'Rafi', Age: 25, Adress: 'Tangail'},
+    ];
+});
