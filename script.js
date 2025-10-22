23ae5de831c075b31e8fd56c54a3782aa81ddf18
Sakibul Hasan Rafi <iamshrafi@gmail.com>
2025-10-22 12:04:26 +0600

    script running

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
-$(document).ready(function(){
-
+$(document).ready(function () {
     const user = [
-        {Name: 'Rafi', Age: 25, Adress: 'Tangail'},
-        {Name: 'Rafi', Age: 25, Adress: 'Tangail'},
-        {Name: 'Rafi', Age: 25, Adress: 'Tangail'},
-        {Name: 'Rafi', Age: 25, Adress: 'Tangail'},
-        {Name: 'Rafi', Age: 25, Adress: 'Tangail'},
-        {Name: 'Rafi', Age: 25, Adress: 'Tangail'},
-        {Name: 'Rafi', Age: 25, Adress: 'Tangail'},
-        {Name: 'Rafi', Age: 25, Adress: 'Tangail'},
-        {Name: 'Rafi', Age: 25, Adress: 'Tangail'},
-        {Name: 'Rafi', Age: 25, Adress: 'Tangail'},
+        { Name: 'Rafi', Age: 25, Adress: 'Tangail' },
+        { Name: 'Rafi', Age: 25, Adress: 'Tangail' },
+        { Name: 'Rafi', Age: 25, Adress: 'Tangail' },
+        { Name: 'Rafi', Age: 25, Adress: 'Tangail' },
+        { Name: 'Rafi', Age: 25, Adress: 'Tangail' },
+        { Name: 'Rafi', Age: 25, Adress: 'Tangail' },
+        { Name: 'Rafi', Age: 25, Adress: 'Tangail' },
+        { Name: 'Rafi', Age: 25, Adress: 'Tangail' },
+        { Name: 'Rafi', Age: 25, Adress: 'Tangail' },
+        { Name: 'Rafi', Age: 25, Adress: 'Tangail' },
     ];
+
+    const tableBody = $('#tableBody');
+
+    user.forEach(u => {
+        const row = `<tr>
+            <td>${u.Name}</td>
+            <td>${u.Age}</td>
+            <td>${u.Adress}</td>
+        </tr>`;
+        tableBody.append(row);
+    });
+
+    function renderTable(data) {
+        const $tbody = $('#tableBody').empty();
+        data.forEach(row => {
+            $tbody.append(`<tr><td>${row.Name}</td><td>${row.Age}</td><td>${row.Adress}</td></tr>`);
+        });
+    }
+
+
+
+
+
 });
