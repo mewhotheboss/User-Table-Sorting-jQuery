9df0496f90c22592217d238633b2fbcfd247dcf5
Sakibul Hasan Rafi <iamshrafi@gmail.com>
2025-10-22 17:12:07 +0600

    done

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 $(document).ready(function () {
     const users = [
-        { Name: 'A', Age: 20, Adress: 'Oangail' },
-        { Name: 'B', Age: 22, Adress: 'Uangail' },
-        { Name: 'D', Age: 24, Adress: 'Tangail' },
-        { Name: 'G', Age: 26, Adress: 'Qangail' },
-        { Name: 'W', Age: 28, Adress: 'Hangail' },
-        { Name: 'X', Age: 21, Adress: 'Bangail' },
-        { Name: 'O', Age: 30, Adress: 'Pangail' },
-        { Name: 'Q', Age: 36, Adress: 'Zangail' },
-        { Name: 'T', Age: 60, Adress: 'Vangail' },
-        { Name: 'M', Age: 99, Adress: 'Wangail' },
+        { Name: 'Rafi Shaheb', Age: 25, Adress: 'Tangail' },
+        { Name: 'Tanvir Shaheb', Age: 23, Adress: 'Thonthonia' },
+        { Name: 'Alamin Shaheb', Age: 24, Adress: 'Bogura' },
+        { Name: 'Roni Shaheb', Age: 26, Adress: 'Satmatha' },
+        { Name: 'Soron Shaheb', Age: 25, Adress: 'Rahman Nagar' },
+        { Name: 'Mahinur Shaheb', Age: 27, Adress: 'Bogura' },
+        { Name: 'Robbani Shaheb', Age: 30, Adress: 'Sariyakandi' },
+        { Name: 'Niamul Shaheb', Age: 30, Adress: 'Sariyakandi' },
+        { Name: 'Nazrul Shaheb', Age: 28, Adress: 'Satmatha' },
+        { Name: 'Biplop Shaheb', Age: 26, Adress: 'Thonthonia' },
     ];
 
     const tableBody = $('#tableBody');
 
     function insertData(data) {
-        // tableBody.empty();
+        tableBody.empty();
         data.forEach(user => {
             const row = `<tr>
                 <td>${user.Name}</td>
@@ -33,7 +33,7 @@ $(document).ready(function () {
         ascending: true
     };
 
-    $('userTable th').on('click', function(){
+    $('#userTable th').on('click', function(){
         const column  = $(this).data('column');
 
         if (sortColumn.column === column) {
@@ -42,6 +42,21 @@ $(document).ready(function () {
             sortColumn.column = column;
             sortColumn.ascending = true;
         }
+
+        const sortedData = [...users].sort((a,b) =>{
+            const valA = a[column];
+            const valB = b[column];
+
+            if(typeof valA === 'number' && typeof valB === 'number' ){
+                return sortColumn.ascending ? valA-valB : valB-valA;
+            }else {
+                return sortColumn.ascending
+                    ? valA.toString().localeCompare(valB.toString())
+                    : valB.toString().localeCompare(valA.toString());
+            }
+        });
+
+        insertData(sortedData);
 
     });
 
